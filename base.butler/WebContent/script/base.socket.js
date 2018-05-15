570aefcbecd485c175f31ad1f7769c7d8effa95a
HongSeongho <HongSeongho@DESKTOP-BP67E7R>
2018-05-15 11:18:27 +0900

    소켓 기능 추가

--- base.butler/WebContent/script/base.socket.js
+++ base.butler/WebContent/script/base.socket.js
@@ -1,13 +1,20 @@
 /*! Copyright (c) 2018 munchkin team
  * SourceName: base.socket
- * Version: 0.0.1
- * SnapshotDate: 2018.03.15
+ * Version: 0.0.2
+ * SnapshotDate: 2018.05.15
  * 
  * Developer's list
  * - seongho, hong
  * 
  * MIT License(http://www.opensource.org/licenses/mit-license.php)
  */
+
+var baseSocketArray = [];
+var baseSocketLoop;
+var baseSocketFunctionLoop = [];
+
+var baseSocketHeartbeatTime = 2000;
+var baseSocketHeartbeatLimitTime = 5000;
 
 (function($) {
 	jQuery.extend(base, {
@@ -84,9 +91,97 @@
 			var _client = Stomp.over(_ws);
 			
 			var _connectCallback = function(){
-				console.log("_connectCallback~~");
+//				console.log("_connectCallback~~");
+				
+				
 				_client.subscribe($option.destination, function(data){
-					callback(data);
+					
+					var body = JSON.parse(data.body);
+					if(body.type === 'heartbeat'){
+						
+						
+						var _isEmptyData = true;
+						
+						for(var i = 0 ; i < baseSocketArray.length ; i ++){
+							console.log(baseSocketArray[i]);
+							var _bodyData = JSON.parse(baseSocketArray[i].data);
+							if(baseSocketArray[i].userPId === body.userPId 
+									&& 
+								baseSocketArray[i].destination === body.destination 
+									&&
+								_bodyData.type === body.object 
+							){
+//								console.log("날짜를 갱신합니다.");
+								baseSocketArray[i].sessionDate = new Date();
+								_isEmptyData = false;
+								return;
+							}
+						}
+						
+						if(_isEmptyData){
+							var _arrayData = {
+									userPId : body.userPId,
+									userName : body.userName,
+									destination : body.destination,
+									type : body.object ,
+									sessionDate : new Date(),
+									data : JSON.stringify(body.data)
+							};
+							baseSocketArray.push(_arrayData);
+							
+							if(callback){
+								callback(body);
+							}
+
+						}
+						
+					}else if(body.type === 'refresh' && callback){
+						callback(body);
+					}else if(body.type === 'writing' && callback){
+						//참여자 정보 추가하기
+						
+						if(isIntegerValid(body.userPId)){
+							
+							var _isDuplication = false;
+							for(var i = 0 ; i < baseSocketArray.length ; i++){
+								if(
+									baseSocketArray[i].userPId === body.userPId
+										&&
+									baseSocketArray[i].type === 'writing'
+								){
+									_isDuplication = true;
+									return;
+								}
+							}
+							if(!_isDuplication){
+								var _arrayData = {
+										userPId : body.userPId,
+										userName : body.userName,
+										destination : $option.destination,
+										type : 'writing',
+										sessionDate : new Date(),
+										data : data.body
+								};
+								baseSocketArray.push(_arrayData);
+							}
+							
+							stopSocketLoop();
+							startSocketLoop($option, callback);
+							
+						}else{
+							base.modal('userPId 정보가 필요합니다.');
+							return;
+						}
+						
+						callback(body);
+					}else if(body.type === 'written' && callback){
+						//write heart-beat가 끝나서 굳이 지울필요가 없음.
+						if(callback)
+							callback(body);
+					}else{
+						if(callback)
+							callback(data);
+					}
 				})
 			}
 			var _errorCallback = function(x){
@@ -95,10 +190,120 @@
 			};
 
 			_client.connect($option.userName, $option.password, _connectCallback, _errorCallback, $option.virtualHost);
+
+			
+
+			startSocketLoop($option, callback);
 			
 			return _client;
 			
 		},
+
+	    /**
+	     * @desc : 이벤트 :: 새로고침 / 구독하고 있는 페이지에 대해 새로고침 명령어를 전달한다. 
+	     * @date : 2018.05.14
+	     * @author: 홍성호
+	     * @support : 
+	     * 	IE 10+
+	     * @param :
+	     * 		명칭			기본값		필수여부	타입			설명
+	     * client								R		string			base.socket으로 부터 반환된 connection 정보
+	     * destination							R		string			websocket 서버의 목적지 정보
+	     * 
+	     * @sample : 
+	     * 	
+	     * base.socketEventRefresh({	
+         *		client : socketClient,
+         *      destination : '/topic/test',
+	     * });	
+	     * 
+	     * @required : 
+	     * 	- jquery-3.2.1.js
+	     * 	- sockjs-1.1.4.js
+	     * 	- stomp.js
+	     * @optional :
+	     * @return :
+	     * @update : 
+	     * 	일시		이름	변경내용
+	     */
+		socketEventRefresh : function($option){
+			if($option.client && $option.client.connected){
+				
+				var _socketDefault = {
+					type : 'refresh'
+				}
+
+		        var _socketParam = $.extend({}, _socketDefault, $option.data);
+				
+				$option.client.send($option.destination, {"content-type":"text/plain"}, JSON.stringify(_socketParam));
+			}
+		},
+
+		socketEventWriting : function($option){
+			if($option.client && $option.client.connected){
+				
+				var _socketDefault = {
+					type : 'writing'
+				}
+
+				var _socketParam = $.extend({}, _socketDefault, $option.data);
+
+				var _loop = setInterval( function(){
+					var _heartbeat = {
+						type : 'heartbeat',
+						object : 'writing',
+						userPId : $option.data.userPId,
+						userName : $option.data.userName,
+						destination : $option.destination,
+						data : _socketParam
+					}
+					$option.client.send($option.destination, {"content-type":"text/plain"}, JSON.stringify(_heartbeat));
+				}, baseSocketHeartbeatTime);
+				
+				var loopData = {
+					type : 'writing',
+					userPId : $option.data.userPId,
+					destination : $option.destination,
+					data : _loop
+				}
+				baseSocketFunctionLoop.push(loopData);
+				
+				$option.client.send($option.destination, {"content-type":"text/plain"}, JSON.stringify(_socketParam));
+			}
+		},
+
+		socketEventWritten : function($option){
+			if($option.client && $option.client.connected){
+				//루프를 끕니다.
+				
+				console.log('루프를 끕니다');
+				for(var i = 0 ; i < baseSocketFunctionLoop.length ; i++){
+					
+					if( 
+							baseSocketFunctionLoop[i].userPId === $option.data.userPId
+								&&
+							baseSocketFunctionLoop[i].destination === $option.destination
+								&&
+							baseSocketFunctionLoop[i].type === 'writing'
+					){
+						var _loop = baseSocketFunctionLoop[i].data;
+						clearInterval(_loop);
+					}
+				}
+				
+				//현재 사용자가 수정중이라면, 수정중을 뻅니다.
+				
+				
+				var _socketDefault = {
+					type : 'written'
+				}
+
+				var _socketParam = $.extend({}, _socketDefault, $option.data);
+				
+				$option.client.send($option.destination, {"content-type":"text/plain"}, JSON.stringify(_socketParam));
+			}
+		},
+		
 
 	    /**
 	     * @desc : 웹소켓을 사용하여 설정된 destination을 구독하는 클라이언트에게 메시지를 전달한다.
@@ -137,3 +342,18 @@
 		}
 	})
 })(jQuery)
+
+
+function startSocketLoop($option, callback){
+	baseSocketLoop = setInterval( function(){
+		for(var i = 0 ; i < baseSocketArray.length; i++){
+			if( ( (new Date()) - baseSocketArray[i].sessionDate) > 5000 ){
+				baseSocketArray.splice(i,1);
+				if(callback) callback();
+			}
+		}
+	}, baseSocketHeartbeatLimitTime);
+}
+function stopSocketLoop(){
+	clearInterval(baseSocketLoop);
+}
